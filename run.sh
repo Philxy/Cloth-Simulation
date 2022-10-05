2f213ee41a7a77fdab45b560b3180bb3e7922123
philxy <philxyphilxy@gmail.com>
2022-10-05 13:43:04 +0200

    removed script

--- run.sh
+++
@@ -1,4 +0,0 @@
-g++ -g -c main.cpp -I/home/phil/Documents/Programming/SFML-2.5.1/include 
-g++ -g -c ./src/*.cpp
-g++ -g -o sfml-app  *.o -L/home/phil/Documents/Programming/SFML-2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system
-./sfml-app
